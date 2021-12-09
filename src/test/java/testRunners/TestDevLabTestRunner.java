eb9b6a0952bebd5f32e1804976459c8700bdaad3
Ivane Tsiklauri <itsiklauri@smartsource.ge>
2021-12-09 18:04:48 +0400

    fix: testrunner

--- src/test/java/testRunners/TestDevLabTestRunner.java
+++ src/test/java/testRunners/TestDevLabTestRunner.java
@@ -4,12 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        glue = {"src/test/java/steps"},
+        glue = {"/steps"},
         features = {"src/test/resources/features/TestDevLab.feature"},
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
-                "json:target/cucumber-reports/cucumber.json",
-                "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"
+                "json:target/cucumber-reports/cucumber.json"
         }
 
 )
